702282029d960810a486637f7ccef0696a939a95
Gabriel <gabrielaugusto9718@gmail.com>
2020-02-06 11:45:35 +0100

    Teste1
    Mensagem pra ver se deu no PC do Ariston

---
+++ Dersodre/Dersodre.ino
@@ -0,0 +1,11 @@
+//Essa mensagem tem que ser exibida no PC do Ariston.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
