d5210d38848fb8da2fe3da71ad8f62e5a5dff0ea
Gabriel <gabrielaugusto9718@gmail.com>
2020-02-11 15:08:10 +0100

    [Adding definies]
    Adding definies with the goal of make it easier to make futures modifications.

--- Dersodre/Dersodre.ino
+++ Dersodre/Dersodre.ino
@@ -13,7 +13,8 @@
 #define pot_2 B10101000
 #define pot_3 B10101100
 
-#define step_voltage 0.078125 //5V divided per 64 (wiper resolution)
+#define voltage_input 5 //Value of the voltage input of the output of arduino applied to the voltage division
+#define number_of_step_DP 64 //The number of steps from the digital potentiometer
 
 /*Action: write in the selected potentiometer a value of resistence by selection the wipe position
  *Inputs: 
@@ -37,6 +38,7 @@ void change_R_DP (uint8_t which_Pot, uint8_t wiper_value_selection){
  * Return: --
  */
 void generate_ramp (uint16_t deltaT, float deltaV){
+  float step_voltage = float(voltage_input)/float(number_of_step_DP); //This calculate the resolution of the voltage that can be applied.
   int step_wiper=deltaV/step_voltage; //Calculate the step that the wiper must increase
   uint8_t wiper_position=0; //New variable to control the position of the wiper by adding the step
   round(step_wiper); //Round the step, as it just can be a integer number between 0-64
@@ -44,8 +46,8 @@ void generate_ramp (uint16_t deltaT, float deltaV){
       change_R_DP(pot_0, wiper_position); //Variate the resistence of the selected potentiometer by selecting the position of the wiper
       wiper_position=wiper_position+step_wiper; //Increasinig the wiper's position according to the step of the definied by the user
       delay(deltaT);
-  }while(wiper_position<64);
-  change_R_DP(pot_0, 0);
+  }while(wiper_position<number_of_step_DP);
+  change_R_DP(pot_0, 0); //Not shure if this should keep here: when the measurement comes to the end, the wiper goes to position 0.
   delay(deltaT);
 }
 
@@ -53,7 +55,7 @@ void setup() {
   // put your setup code here, to run once:
   Wire.begin(); //Boot of the library and setting the arduino as master. This is a default option: when there is nothing written inside the parenthesis, the device is a master.
   Serial.begin(9600);
-  generate_ramp(500, 0.234375);
+  generate_ramp(1000, 0.234375);
 }
 
 void loop() {
